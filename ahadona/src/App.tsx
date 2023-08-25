8430ff7c485880e95c48fac793071a7542f75590
adrianheroadona <adrianheroadona@gmail.com>
2023-08-25 14:37:13 +0800

    init

---
+++ ahadona/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MouseMove from './components/mouse'; // Make sure the path is correct
+
+function App() {
+  return (
+    <div>
+      <MouseMove/>
+      <h1 className='name'>ADRIAN ADONA</h1>
+    </div>
+  );
+}
+
+export default App
---
+++ ahadona/src/components/mouse.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+function MouseMove() {
+  useEffect(() => {
+    const blob = document.querySelector('.cursor-follower') as HTMLElement;
+
+    function updateCursorPosition(event: PointerEvent) {
+      const { clientX, clientY } = event;
+
+      blob.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+      }, { duration: 1000, fill: "forwards"});
+    }
+
+    document.body.addEventListener('pointermove', updateCursorPosition);
+
+    return () => {
+      document.body.removeEventListener('pointermove', updateCursorPosition);
+    };
+  }, []);
+
+  return (
+    <div className="cursor-follower">
+      {/* You can add other content here if needed */}
+    </div>
+  );
+}
+
+export default MouseMove;
